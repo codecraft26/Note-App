0509a639f10173b7779fddf66a5562788c909c7e
codecraft26 <codescraft26@gmail.com>
2022-01-24 14:29:35 +0530

    changing the view model

--- app/src/main/java/dev/aman/noteapp/MainActivity.kt
+++ app/src/main/java/dev/aman/noteapp/MainActivity.kt
@@ -3,10 +3,18 @@ package dev.aman.noteapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            viewModel=ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel ::class.java)
+        viewModel.allNotes.observe(this, Observer{
+            
+        })
     }
 }
