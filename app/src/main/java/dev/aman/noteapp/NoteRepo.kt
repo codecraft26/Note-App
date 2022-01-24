e9232c45a1207b0e491f1931a5c5578ae231013e
codecraft26 <codescraft26@gmail.com>
2022-01-24 14:29:35 +0530

    make repo for accesing the data

---
+++ app/src/main/java/dev/aman/noteapp/NoteRepo.kt
@@ -0,0 +1,13 @@
+package dev.aman.noteapp
+
+import androidx.lifecycle.LiveData
+
+class NoteRepo(private val noteDao:NoteDao) {
+    val allNotes:LiveData<List<Note>> = noteDao.getAllNotes()
+    suspend fun insert(note:Note){
+        noteDao.insert(note)
+    }
+    suspend fun delete(note:Note){
+        noteDao.delete(note)
+    }
+}
