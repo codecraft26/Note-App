d88a72afeafe9b76d6624055c6d470532e625cd9
codecraft26 <codescraft26@gmail.com>
2022-02-22 01:59:07 +0530

    intial commit

---
+++ app/src/main/java/dev/aman/noteapp/Note.kt
@@ -0,0 +1,15 @@
+package dev.aman.noteapp
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes_table")
+data class Note(
+    @ColumnInfo(name="text")
+    val text: String
+
+)
+{
+    @PrimaryKey(autoGenerate = true) var id = 0
+}
